fa10ecc823bd634c0f62c51e89a000355170f26f
Warren Hernandez <90418549+Warhernandez@users.noreply.github.com>
2023-11-09 16:14:25 -0600

    STUFF SHATTERS (#13)
    IT DOES

--- Assets/Scripts/CompletedScripts/Target.cs
+++ Assets/Scripts/CompletedScripts/Target.cs
@@ -3,10 +3,23 @@
 public class Target : MonoBehaviour
 {
     public int scoreValue = 1; // The score value to give the player when the target is hit.
+    private Explodable Explodable;
 
+    private void Start()
+    {
+        Explodable = GetComponent<Explodable>();
+    }
     public void HitTarget()
     {
         //For handling animations and stuff later down the line.
         gameObject.SetActive(false);
+    }
+
+    public void Shatter()
+    {
+
+        Explodable.explode();
+        ExplosionForce ef = GameObject.FindObjectOfType<ExplosionForce>();
+        ef.doExplosion(transform.position);
     }
 }

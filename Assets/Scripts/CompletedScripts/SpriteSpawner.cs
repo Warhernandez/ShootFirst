a6d24f62130535eafc494bdf83b6c37d7e1fb959
Warren Hernandez <405336@colum.edu>
2023-12-08 15:14:05 -0600

    in class work week 14

---
+++ Assets/Scripts/CompletedScripts/SpriteSpawner.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class SpriteSpawner : MonoBehaviour
+{
+    public GameObject spritePrefab1;
+    public GameObject spritePrefab2;
+    public GameObject specialSpritePrefab;
+
+    public int numberOfSprites = 10;
+    public float spawnRadius = 5f;
+    public Vector2 specialSpriteSpawnPoint = new Vector2(0f, 0f);
+
+    void Start()
+    {
+        SpawnSprites(spritePrefab1, numberOfSprites);
+        SpawnSprites(spritePrefab2, numberOfSprites);
+
+        // Spawn a special sprite at a specific point
+        SpawnSpecialSprite(specialSpritePrefab, specialSpriteSpawnPoint);
+    }
+
+    void SpawnSprites(GameObject prefab, int count)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            Vector2 randomPosition = Random.insideUnitCircle * spawnRadius;
+            Instantiate(prefab, randomPosition, Quaternion.identity);
+        }
+    }
+
+    void SpawnSpecialSprite(GameObject prefab, Vector2 spawnPoint)
+    {
+        Instantiate(prefab, spawnPoint, Quaternion.identity);
+    }
+}
